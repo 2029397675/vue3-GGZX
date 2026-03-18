3ded31b15c8f396cdcc4eac3340d8c552085bf2f
2029397675 <2029397675@qq.com>
2026-03-18 23:13:55 +0800

    feat:集成ElementPlus组件库

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
     <h1>test</h1>
+    <el-button
+      type="primary"
+      icon="edit"
+      size="default"
+      circle
+      @click="1"
+    ></el-button>
   </div>
 </template>
 
-<script lang="ts" setup>
-console.log(1)
-</script>
+<script lang="ts" setup></script>
 
 <style lang="scss" scoped></style>
