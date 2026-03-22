25d31699b5723a8c4bb8c348143736a7b8ebcb75
2029397675 <2029397675@qq.com>
2026-03-22 16:56:37 +0800

    feat:使用mock假接口

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,18 @@
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import axios from 'axios'
+import { onMounted } from 'vue'
+
+onMounted(async () => {
+  const res = await axios.post('/api/user/login', {
+    username: 'admin',
+    password: '1121111'
+  })
+  console.log(res)
+})
+</script>
 
 <style lang="scss">
 div {
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+//引入mock插件
+import { viteMockServe } from 'vite-plugin-mock'
 //引入SVG需要用到的插件
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [
     vue(),
     createSvgIconsPlugin({
       iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
       symbolId: 'icon-[dir]-[name]'
-    })
+    }),
+    command === 'serve' &&
+      viteMockServe({
+        mockPath: 'mock',
+        watchFiles: true
+      })
   ],
   resolve: {
     alias: {
@@ -25,4 +32,4 @@ export default defineConfig({
       }
     }
   }
-})
+}))
