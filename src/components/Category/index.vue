b5744fdad03ad1c9d3b030df50bf700a8d2a63c5
2029397675 <2029397675@qq.com>
2026-08-18 22:09:42 +0800

    feat:属性管理模块静态搭建

--- src/components/Category/index.vue
+++ src/components/Category/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <Category></Category>
     <el-card>
       <el-form :inline="true">
         <el-form-item label="一级分类">
@@ -19,27 +20,9 @@
         </el-form-item>
       </el-form>
     </el-card>
-    <el-card style="margin: 20px 0">
-      <el-button type="primary" icon="Plus">添加属性</el-button>
-      <el-table border style="margin: 10px 0">
-        <el-table-column
-          label="序号"
-          type="index"
-          align="center"
-          width="150px"
-        ></el-table-column>
-        <el-table-column label="属性名称" width="250px"></el-table-column>
-        <el-table-column label="属性值名称"></el-table-column>
-        <el-table-column label="操作" width="250px"></el-table-column>
-      </el-table>
-    </el-card>
   </div>
 </template>
 
 <script lang="ts" setup></script>
 
-<style lang="scss" scoped>
-.el-select {
-  width: 240px;
-}
-</style>
+<style lang="scss" scoped></style>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,10 @@
 import SvgIcon from '@/components/SvgIcon/index.vue'
+import Category from '@/components/Category/index.vue'
 import type { App } from 'vue'
 
 //对外暴露的组件
 
-const allGlobalComponents = { SvgIcon } as const
+const allGlobalComponents = { SvgIcon, Category } as const
 export default {
   install(app: App) {
     //注册全局组件
