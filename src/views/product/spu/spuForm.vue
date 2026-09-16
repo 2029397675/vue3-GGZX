0893a3fa855a762eb24519f055ea4f745691ebae
2029397675 <2029397675@qq.com>
2026-09-16 20:32:14 +0800

    feat:完成收集新增销售属性

--- src/views/product/spu/spuForm.vue
+++ src/views/product/spu/spuForm.vue
@@ -44,13 +44,30 @@
       </el-dialog>
     </el-form-item>
     <el-form-item label="SPU销售属性">
-      <el-select style="width: 200px">
-        <el-option label="品牌1" value="1"></el-option>
-        <el-option label="品牌2" value="2"></el-option>
-        <el-option label="品牌3" value="3"></el-option>
+      <el-select
+        v-model="saleAttrIdAndValueName"
+        :placeholder="
+          unSelectSaleAttr.length
+            ? `还未选择${unSelectSaleAttr.length}个`
+            : '无'
+        "
+        style="width: 200px"
+      >
+        <el-option
+          v-for="item in unSelectSaleAttr"
+          :key="item.id"
+          :label="item.name"
+          :value="`${item.id}:${item.name}`"
+        ></el-option>
       </el-select>
-      <el-button style="margin-left: 10px" type="primary" icon="Plus">
-        添加销售属性
+      <el-button
+        :disabled="!saleAttrIdAndValueName"
+        style="margin-left: 10px"
+        type="primary"
+        icon="Plus"
+        @click="addSaleAttr"
+      >
+        添加属性
       </el-button>
       <!-- tanle展示销售属性与属性值 -->
       <el-table border style="margin: 10px 0" :data="saleAttr">
@@ -102,7 +119,7 @@
 
 <script lang="ts" setup>
 import { ElMessage, type UploadUserFile } from 'element-plus' //导入上传组件的类型
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import {
   reqAllSaleAttr,
   reqAllTradeMark,
@@ -120,6 +137,7 @@ import type {
   SaleAttr,
   HasSaleAttr
 } from '@/api/product/spu/type'
+
 //父组件的自定义事件
 const emit = defineEmits(['changeScene'])
 //点击取消按钮
@@ -129,7 +147,7 @@ const cancel = () => {
 //控制对话框的显示与隐藏
 const dialogVisible = ref<boolean>(false)
 //对话框中预览图片的URL
-const dialogImageUrl = ref<string>('') //图片URL
+const dialogImageUrl = ref<string>('123') //图片URL
 //存放数据
 const allTrademark = ref<Trademark[]>([])
 const imgList = ref<SpuImage[]>([])
@@ -144,6 +162,8 @@ const SpuParams = ref<SpuData>({
   spuImageList: [], //商品图片列表
   spuSaleAttrList: [] //商品销售属性列表
 })
+//收集还未选择属性的id和name
+const saleAttrIdAndValueName = ref<string>('')
 //子组件方法
 const initHasSpuData = async (spu: SpuData) => {
   //存储已有的SPU对象，以便在模版中展示
@@ -187,6 +207,31 @@ const handleUpload = (file: any) => {
     return false
   }
 }
+//计算出当前SPU还未拥有的销售属性
+const unSelectSaleAttr = computed(() => {
+  //全部的销售属性：颜色、尺寸、版本0
+  let unSelectAttr = allSaleAttr.value.filter(item => {
+    return saleAttr.value.every(saleItem => {
+      return item.name != saleItem.saleAttrName
+    })
+  })
+  return unSelectAttr
+})
+//添加销售属性的方法
+const addSaleAttr = () => {
+  //结构成服务器需要的格式
+  const [baseSaleAttrId, saleAttrName] = saleAttrIdAndValueName.value.split(':')
+  let newSaleAttr: SaleAttr = {
+    baseSaleAttrId: Number(baseSaleAttrId),
+    saleAttrName: saleAttrName,
+    spuSaleAttrValueList: []
+  }
+  //追加到数组中
+  saleAttr.value.push(newSaleAttr)
+  //清空输入框
+  saleAttrIdAndValueName.value = ''
+}
+
 defineExpose({ initHasSpuData })
 </script>
 
